fdc5db5357a28c4eb95e9e7e41d4d7a081fdefb2
fuckyou <fuckyou@example.com>
2025-06-04 15:01:47 +0545

    deleted a line

--- config.def.h
+++ config.def.h
@@ -90,8 +90,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      spawn,          SHCMD("pkill dwm")},
-	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
