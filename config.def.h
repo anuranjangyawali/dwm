bb219026868cb652ddb3777ed639fe0745d85d79
yourname <you@example.com>
2025-11-29 19:02:20 +0545

    changed fonts

--- config.def.h
+++ config.def.h
@@ -5,8 +5,9 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=13", "fontawesome:pixelsize=10:antialias=true:autohint=true", "JetBrainsMono Nerd Font:pixelsize=18:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrains Mono:size=13";
+static const char *fonts[]          = { "InconsolataNerdFontMono:size=17" };
+static const char dmenufont[]       = "InconsolataNerdFontMono:size=17";
+
 
 #include "/home/kiwi/.cache/wal/colors-wal-dwm.h"
 
